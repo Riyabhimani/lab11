1ffc9253131c9c22d241c5059ef192856e14ef9d
Riyabhimani <146011638+Riyabhimani@users.noreply.github.com>
2023-10-03 09:23:18 +0530

    Add files via upload

---
+++ asciicharacters.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+    void main(){
+    	int i,j;
+    	//printf("Enter n:");
+    	//scanf("%d",&n);
+    	for(i=65;i<=90;i++){
+    		printf("%c=%d\n",i,i);
+    	}
+    		for(j=97;j<=122;j++){
+    			printf("%c=%d",j,j);
+			}
+		
+	}
